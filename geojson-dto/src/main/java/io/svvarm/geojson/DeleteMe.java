ffac9733294732251377e9aba317f01e603ce183
Baljit Singh <baljit.singh.pro@gmail.com>
2021-10-12 23:07:18 -0400

    Check for code coverage

---
+++ geojson-dto/src/main/java/io/svvarm/geojson/DeleteMe.java
@@ -0,0 +1,9 @@
+package io.svvarm.geojson;
+
+public class DeleteMe {
+
+  // only exists to check code coverage
+  public static boolean isNull(final Object object) {
+    return object == null;
+  }
+}
