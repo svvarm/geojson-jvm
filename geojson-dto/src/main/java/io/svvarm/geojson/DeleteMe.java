77ef9d961f5460e4a7875c4285cd0044a76979dc
Baljit Singh <baljit.singh.pro@gmail.com>
2021-10-13 17:52:51 -0400

    Enhace global Gradle build file

--- geojson-dto/src/main/java/io/svvarm/geojson/DeleteMe.java
+++
@@ -1,9 +0,0 @@
-package io.svvarm.geojson;
-
-public class DeleteMe {
-
-  // only exists to check code coverage
-  public static boolean isNull(final Object object) {
-    return object == null;
-  }
-}
